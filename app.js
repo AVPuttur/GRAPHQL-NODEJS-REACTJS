4f4cb11752782eb24e06982c18bc33fd5b58a848
Varun Puttur <avputtur@Varuns-MacBook-Pro.local>
2019-12-14 17:31:09 +0400

    data types added

--- app.js
+++ app.js
@@ -5,23 +5,42 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+//Event array
+const aEvents = [];
+
 //Middleware to pass json
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
 	//return lists of events
 	//a function, name as argument
-	//only query data
-	//to manipulate data - create/delete/update
+	//query - only query data
+	//mutation - to manipulate data - create/delete/update
+	// ! - cannot be empty
 
 	schema:buildSchema(`
 
+		type Event {
+			_id: ID!
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+
+		input EventInput {
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+
 		type RootQuery {
-			events: [String!]! 
+			events: [Event!]! 
 		}
 
 		type RootMutation {
-			createEvent(name :String): String 
+			createEvent(eventInput: EventInput): Event 
 		}
 
 		schema {
@@ -34,13 +53,24 @@ app.use('/graphql', graphqlHttp({
 	//also known as the resolver in graphql
 	rootValue: {
 		events: () => { //query
-			return ['Romantic dinner', 'Sailing', 'Gardening'];
+
+			//return ['Romantic dinner', 'Sailing', 'Gardening'];
+			return aEvents;
 		}, 
 
 		createEvent: (args) => {
-			const eventName = args.name;
-			return eventName;
-		}
+			//const eventName = args.name;
+			const event = {
+				_id: Math.random().toString(),
+				title: args.eventInput.title,
+				description: args.eventInput.description,
+				price: +args.eventInput.price,
+				date: args.eventInput.date
+			};
+			aEvents.push(event);
+			return event;
+
+			}
 	},
 
 	graphiql: true, //graphql debuging
